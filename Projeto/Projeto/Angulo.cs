60ef438d370e60390eef14feb28e66cdb83a0499
Jorge Dorio <jorgegabrielbd.jj@gmail.com>
2023-10-11 01:07:59 -0300

    feat: crud estações

---
+++ Projeto/Projeto/Angulo.cs
@@ -0,0 +1,13 @@
+﻿namespace Projeto;
+
+public class Angulo
+{
+  public int Graus { get; set; }
+  public int Minutos { get; set; }
+  public int Segundos { get; set; }
+
+  public override string ToString()
+  {
+    return $"{Graus}º {Minutos}' {Segundos}''";
+  }
+}
---
+++ Projeto/Projeto/Estacao.cs
@@ -0,0 +1,19 @@
+﻿namespace Projeto;
+
+public class Estacao
+{
+  public Estacao()
+  {
+    AngEstacao = new Angulo();
+  }
+
+  public Angulo AngEstacao { get; set; }
+  public Angulo Azimute { get; set; }
+  public float Distancia { get; set; }
+  public char Deflexao { get; set; }
+
+  public override string ToString()
+  {
+    return $"{AngEstacao} - {Distancia} - {Deflexao}";
+  }
+}
