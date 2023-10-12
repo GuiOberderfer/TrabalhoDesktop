5c95b473e675d1f2c6e6cbeb9c2e188a4f994ce3
GuiOberderfer <147074072+GuiOberderfer@users.noreply.github.com>
2023-10-11 21:22:05 -0300

    Update TelaPoligonal.cs

--- Projeto/Projeto/TelaPoligonal.cs
+++ Projeto/Projeto/TelaPoligonal.cs
@@ -2,28 +2,32 @@ namespace Projeto;
 
 public partial class Poligonal
 {
-     void ImprimeTelaListar()
+    void ImprimeTelaListar()
     {
-      //todo ajustar tela de listagem
-      //todo adicionar paginação, limitar itens a 10
-      //todo utilizar funções de espaçamento
-      Console.Clear();
-      Console.WriteLine("Engenharia Cartográfica - Sistema de Poligonais");
-      Console.WriteLine($"Poligonal: {Descricao}");
-      Console.WriteLine("===================================================");
-      Console.WriteLine("Estação\tÂngulo lido\tDeflexão\tDistância(m)\tAzimute");
-      Console.WriteLine("===================================================");
-      
-      int estacaoNum = 1;
-      foreach (var estacao in Estacoes)
-      {   
-          CalcAzimute(estacao);
-          Console.WriteLine($"{estacaoNum:D4}\t{estacao.AngEstacao}\t{estacao.Deflexao}\t{estacao.Distancia}\t{estacao.Azimute}");
-          estacaoNum++;
-      }
+        //todo ajustar tela de listagem
+        //todo adicionar paginação, limitar itens a 10
+        //todo utilizar funções de espaçamento
+        Console.Clear();
+        Console.WriteLine("Engenharia Cartográfica             Sistema de Poligonais             Data: " + DateTime.Now.ToString("dd/MM/yyyy"));
+        Console.WriteLine("======================================================================================");
+        Console.WriteLine($"Poligonal: {Descricao}");
+        Console.WriteLine("--------------------------------------------------------------------------------------");
 
-      Console.WriteLine("===================================================");
-      Console.WriteLine($"Perímetro: {Perimetro():F2} metros");
-      Console.WriteLine("Pressione <Esc> para Sair | <F1> Inserir | <F2> Alterar | <F3> Excluir | <CTRL + S> Salvar | <PgDn> | <PgUp>");
+        // Cabeçalho da tabela
+        Console.WriteLine("Estação      Ângulo lido      Deflexão       Distância(m)     Azimute");
+        Console.WriteLine("======================================================================================");
+
+        int estacaoNum = 1;
+        foreach (var estacao in Estacoes)
+        {
+            // Use formatação para alinhar os valores
+            Console.WriteLine($"{estacaoNum:D4}         {estacao.AngEstacao,-14:F2}     {estacao.Deflexao,-14:F2}   {estacao.Distancia,-14:F2}     {estacao.Azimute,-14:F2}");
+            estacaoNum++;
+        }
+
+        Console.WriteLine("======================================================================================");
+        Console.WriteLine($"Perímetro: {Perimetro():F2} metros");
+        Console.WriteLine("Pressione <Esc> para Sair | <F1> Inserir | <F2> Alterar | <F3> Excluir | <CTRL + S> Salvar | <PgDn> | <PgUp>");
     }
+}
 }
